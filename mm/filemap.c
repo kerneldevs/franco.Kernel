0221563c95c96c0da4be7fc7b2491202f6af95ac
francisco <francisco@francisco.(none)>
2011-08-30 20:51:28 +0100

    [mm] various memory bugfixes and tweaks

--- mm/filemap.c
+++ mm/filemap.c
@@ -2198,7 +2198,7 @@ struct page *grab_cache_page_write_begin(struct address_space *mapping,
 		gfp_notmask = __GFP_FS;
 repeat:
 	page = find_lock_page(mapping, index);
-	if (likely(page))
+	if (page)
 		goto found;
 
 	page = __page_cache_alloc(mapping_gfp_mask(mapping) & ~gfp_notmask);
